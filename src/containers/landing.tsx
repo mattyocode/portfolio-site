5344117e0eb60845a073e520644ab5799f5ee8cf
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-04 18:08:25 +0000

    add links to landing

--- src/containers/landing.tsx
+++ src/containers/landing.tsx
@@ -43,6 +43,7 @@ export default function LandingSection(): JSX.Element {
             </h2>
           </LandingCopy>
         </LandingCentered>
+        {/* */}
         <ContactIcons links={ContactLinks} />
       </FullBleedWrapper>
     </Section>
