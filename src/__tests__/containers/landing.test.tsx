a25f20cbc339e11bf486f75ef340b0e3a9a52f27
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-07 18:57:50 +0000

    jest config set up

---
+++ src/__tests__/containers/landing.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import LandingSection from '../../containers/landing';
+import { SectionNavRef } from '../../types';
+
+describe('<LandingSection />', () => {
+  const mockRef = jest.fn();
+  beforeEach(() => {
+    Object.defineProperty(global, 'ResizeObserver', {
+      writable: true,
+      value: jest.fn().mockImplementation(() => ({
+        observe: jest.fn(() => 'Mocking works'),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+      })),
+    });
+  });
+  test('renders <LandingSection/>', () => {
+    render(<LandingSection navRef={mockRef} />);
+    expect(screen.getByText(/full stack developer/i)).toBeInTheDocument();
+  });
+});
