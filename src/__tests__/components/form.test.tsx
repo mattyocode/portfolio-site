d6950e82099647faaf27bee1992aeff4210ff8fe
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-11 11:36:10 +0000

    test: form button disabled with incomplete fields

--- src/__tests__/components/form.test.tsx
+++ src/__tests__/components/form.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import ContactForm from '../../components/form';
 
 describe('<ContactForm /> tests', () => {
-  // beforeEach(() => {
-  //   const submitMock = jest.fn();
-  //   render(<ContactForm submitFn={submitMock} />);
-  // });
-
   test('renders blank <ContactForm />', () => {
     const submitMock = jest.fn();
     render(<ContactForm submitFn={submitMock} />);
@@ -23,13 +24,13 @@ describe('<ContactForm /> tests', () => {
   });
 
   test('submits with name, email, and message', async () => {
+    const promise = Promise.resolve();
     const submitMock = jest.fn();
     render(<ContactForm submitFn={submitMock} />);
 
     userEvent.type(screen.getByLabelText(/name/i), 'Matt');
     userEvent.type(screen.getByLabelText(/email/i), 'hi@test.com');
     userEvent.type(screen.getByLabelText(/message/i), 'Hello!');
-
     userEvent.click(screen.getByRole('button', { name: /send/i }));
 
     await waitFor(() => {
@@ -38,6 +39,22 @@ describe('<ContactForm /> tests', () => {
         email: 'hi@test.com',
         message: 'Hello!',
       });
+    });
+  });
+
+  test('submit button disabled when fields incomplete', async () => {
+    const submitMock = jest.fn();
+    render(<ContactForm submitFn={submitMock} />);
+
+    act(() => {
+      userEvent.type(screen.getByLabelText(/name/i), 'Matt');
+      userEvent.type(screen.getByLabelText(/email/i), 'hi@test.com');
+    });
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: /send/i })).toHaveAttribute(
+        'disabled'
+      );
     });
   });
 });
