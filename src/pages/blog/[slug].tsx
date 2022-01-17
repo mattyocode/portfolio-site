69520355d6d43537737f27c269a3ace82a5c427d
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-17 15:35:37 +0000

    reduce bundle size

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export default function () {
+export default function BlogPost() {
   const router = useRouter();
   console.log('router >', router.query.slug);
   return (
