7c8e97db580e47682c48fd762a060d3b65269c9d
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-26 21:12:16 +0000

    change scroll logic to proximity

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -68,23 +68,23 @@ export const ScrollLink: React.FC<ScrollLinkProps> = ({
         e.preventDefault();
         const destination = document.querySelector(href as string);
         if (destination) {
-          // document.body.style.scrollSnapType = 'none';
-          // document.getElementById('pagewrapper')!.style.scrollSnapType = 'none';
+          document.body.style.scrollSnapType = 'none';
+          document.getElementById('pagewrapper')!.style.scrollSnapType = 'none';
           destination.scrollIntoView({
             behavior: 'smooth',
             block: 'center',
             inline: 'center',
           });
-          // setTimeout(() => {
-          //   document.body.style.scrollSnapType = 'y proximity';
-          //   document.getElementById('pagewrapper')!.style.scrollSnapType =
-          //     'y proximity';
-          //   destination.scrollIntoView({
-          //     behavior: 'smooth',
-          //     block: 'center',
-          //     inline: 'center',
-          //   });
-          // }, 1000);
+          setTimeout(() => {
+            document.body.style.scrollSnapType = 'y proximity';
+            document.getElementById('pagewrapper')!.style.scrollSnapType =
+              'y proximity';
+            destination.scrollIntoView({
+              behavior: 'smooth',
+              block: 'center',
+              inline: 'center',
+            });
+          }, 1500);
         }
       }
     },
