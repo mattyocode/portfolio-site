89188170e126e2d3ab325fdf07962a4a89f8dd72
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-26 19:06:22 +0000

    enlarge project content

--- src/components/card/styles/card.tsx
+++ src/components/card/styles/card.tsx
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   border: 3px #ff8a65 solid;
   border-radius: 5px;
@@ -32,7 +32,7 @@ export const ContentWrapper = styled.div`
   position: relative;
   margin: 0.5rem;
   height: 12rem;
-  width: 15rem;
+  width: 16rem;
 
   @media screen and (min-width: 350px) and (min-height: 700px) {
     height: 13rem;
@@ -91,11 +91,10 @@ export const BodyCopy = styled.p`
     margin: 0.5rem;
   }
 
-  /* @media screen and (min-width: 450px) and (min-height: 668px) { */
-  @media screen and (min-width: 450px) and (min-height: 730px) {
+  /* @media screen and (min-width: 550px) and (min-height: 730px) {
     font-size: 1rem;
     margin: 0.7rem;
-  }
+  } */
 `;
 
 export const Footer = styled.div`
@@ -108,9 +107,13 @@ export const Footer = styled.div`
   padding: 0.25rem 0.5rem;
 `;
 
+export const Icons = styled.div`
+  display: flex;
+`;
+
 export const Icon = styled.img`
-  width: 20px;
-  height: 20px;
+  width: 25px;
+  height: 25px;
   margin: 5px;
   object-fit: contain;
 
@@ -120,10 +123,10 @@ export const Icon = styled.img`
   }
 `;
 
-export const Icons = styled.div`
-  display: flex;
-`;
-
 export const LinkIcons = styled.div`
   display: flex;
+
+  img {
+    margin: 5px 7px;
+  }
 `;
