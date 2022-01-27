44d21d6ef7bca910317762f9cc865be89a636fde
Matt <63904102+mattyocode@users.noreply.github.com>
2022-01-27 11:13:18 +0000

    import fonts from google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head></Head>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'
+            rel='stylesheet'
+          />
+
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans&family=Source+Sans+Pro:wght@900&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,20 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const FontStyles = createGlobalStyle`
-  /* source-sans-pro-regular - latin */
-@font-face {
-  font-family: 'Source Sans Pro';
-  font-style: normal;
-  font-weight: 400;
-  font-display: swap;
-  src: url('/fonts/source-sans-pro-v18-latin-regular.eot'); /* IE9 Compat Modes */
-  src: local(''),
-       url('/fonts/source-sans-pro-v18-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-       url('/fonts/source-sans-pro-v18-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
-       url('/fonts/source-sans-pro-v18-latin-regular.woff') format('woff'), /* Modern Browsers */
-       url('/fonts/source-sans-pro-v18-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
-       url('/fonts/source-sans-pro-v18-latin-regular.svg#SourceSansPro') format('svg'); /* Legacy iOS */
-}
 /* source-sans-pro-900 - latin */
 @font-face {
   font-family: 'Source Sans Pro';
